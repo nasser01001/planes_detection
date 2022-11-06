c0a719e3be416800d7ffe957cf1f09749e0bcc62
unknown <abdelrahman.i.rezk@gmai.com>
2022-11-06 18:03:35 +0200

    first again

--- aa.py
+++ aa.py
@@ -7,7 +7,6 @@
 
 import streamlit as st
 import torch
-import torchvision.transforms as transforms
 
 from PIL import Image
 from io import *
@@ -19,99 +18,72 @@ import time
 
 
 from pathlib import Path
-import subprocess
-
 
 import sys
-import os
-import json
-# import cv2
-import random
-# sys.path.insert(0, './yolov7')
-# from models.experimental import attempt_load
-# os.system("python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'")
-
-# import detectron2
-# from detectron2.utils.logger import setup_logger
-# setup_logger()
-# from detectron2 import model_zoo
-# from detectron2.engine import DefaultPredictor
-# from detectron2.config import get_cfg
-# from detectron2.utils.visualizer import Visualizer
-# from detectron2.data import MetadataCatalog, DatasetCatalog
-# from detectron2.structures import BoxMode
-# from detectron2.data.datasets import register_coco_instances
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.structures import BoxMode
+from detectron2.data.datasets import register_coco_instances
 
 # import torch
 # torch.__version__
 import torchvision
 import numpy as np
-
+import os, json, cv2, random
 import matplotlib.pyplot as plt
 
-import subprocess
-import sys
+from detectron2.data.datasets import register_coco_instances
 
-
-# if not os.path.isdir("yolov7"):
-#     yolov7_repo_url = "https://github.com/WongKinYiu/yolov7"
-#     os.system(f"git clone {yolov7_repo_url}")
-#     # Fix import errors
-#     for file in [
-#         "yolov7/models/common.py",
-#         "yolov7/models/experimental.py",
-#         "yolov7/models/yolo.py",
-#         "yolov7/utils/datasets.py",
-#     ]:
-#         prepend_text(file, "import sys\nsys.path.insert(0, './yolov7')")
-
-from models.experimental import attempt_load
-# from detectron2.data.datasets import register_coco_instances
-
-# try:
-#     register_coco_instances("my_dataset_train", {}, "./data/_annotations.coco.json", "./data")
+try:
+    register_coco_instances("my_dataset_train", {}, "./data/_annotations.coco.json", "./data")
     
-# except:
-#     print("already registered")
-# my_dataset_train_metadata = MetadataCatalog.get("my_dataset_train")
+except:
+    print("already registered")
+my_dataset_train_metadata = MetadataCatalog.get("my_dataset_train")
     
-transform = transforms.Compose([
-    transforms.PILToTensor()
-])
-# cfg = get_cfg()
-# cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
-# cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
-# cfg.MODEL.DEVICE = 'cpu'
-# cfg.DATASETS.TRAIN = ("my_dataset_train",)
-# cfg.DATALOADER.NUM_WORKERS = 4
-# cfg.MODEL.WEIGHTS = "model_final.pth"
-# cfg.SOLVER.IMS_PER_BATCH = 4
-# cfg.SOLVER.BASE_LR = 0.001
-# cfg.SOLVER.WARMUP_ITERS = 1000
-# cfg.SOLVER.MAX_ITER = 1500 #adjust up if val mAP is still rising, adjust down if overfit
-# cfg.SOLVER.STEPS = (1000, 1500)
-# cfg.SOLVER.GAMMA = 0.05
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+cfg.MODEL.DEVICE = 'cpu'
+cfg.DATASETS.TRAIN = ("my_dataset_train",)
+cfg.DATALOADER.NUM_WORKERS = 4
+cfg.MODEL.WEIGHTS = "model_final.pth"
+cfg.SOLVER.IMS_PER_BATCH = 4
+cfg.SOLVER.BASE_LR = 0.001
+cfg.SOLVER.WARMUP_ITERS = 1000
+cfg.SOLVER.MAX_ITER = 1500 #adjust up if val mAP is still rising, adjust down if overfit
+cfg.SOLVER.STEPS = (1000, 1500)
+cfg.SOLVER.GAMMA = 0.05
 
 
 
 
-# cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 64
-# cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2 #your number of classes + 1
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 64
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2 #your number of classes + 1
 
-# cfg.TEST.EVAL_PERIOD = 500
-# predictor = DefaultPredictor(cfg)
+cfg.TEST.EVAL_PERIOD = 500
+predictor = DefaultPredictor(cfg)
 
 
 
 
 
 ## CFG
-# cfg_model_path = "models/yourModel.pt" 
+cfg_model_path = "models/yourModel.pt" 
 
-# cfg_enable_url_download = True
-# if cfg_enable_url_download:
-#     url = "https://archive.org/download/yoloTrained/yoloTrained.pt" #Configure this if you set cfg_enable_url_download to True
-#     cfg_model_path = f"models/{url.split('/')[-1:]}" #config model path from url name
+cfg_enable_url_download = True
+if cfg_enable_url_download:
+    url = "https://archive.org/download/yoloTrained/yoloTrained.pt" #Configure this if you set cfg_enable_url_download to True
+    cfg_model_path = f"models/{url.split('/')[-1:]}" #config model path from url name
+
+
 
 
 
@@ -133,29 +105,10 @@ def imageInput(src):
             
                 if image_file is not None :
 
-#                     subprocess.run(["python", "yolov7/detect.py"])
-#                     os.system("python yolov7/detect.py --weights yolov7_best.pt --img 416 --conf 0.4 --source {}".format("upload.png"))
-                    #--Display predicton
-#                     subprocess.run([f"{sys.executable}", "yolov7/detect.py --weights yolov7_best.pt --img 416 --conf 0.4 --source {}".format("upload.png")])
-#                     model = torch.hub.load('yolov7','custom',path="yolov7_best.pt",force_reload=True)
-                    #model = attempt_load('yolov7_best.pt', map_location='cpu')
-#                     model.cpu()
-#                     pred = model("upload.png")
-#                     pred.render()  # render bbox in image
 
-#                     for im in pred.imgs:
-#                         im_base64 = Image.fromarray(im)
-#                         im_base64.save("result_v7.png")
-                    model = attempt_load("yolov7_best.pt", map_location='cpu')
-    
-                    img = Image.open("upload.png")
-                    img_tensor = transform(img)
-                    pred = model(img_tensor)
-                    pred.render()  # render bbox in image
-                    for im in pred.ims:
-                        im_base64 = Image.fromarray(im)
-                        im_base64.save("result_v7.png")
-                    img_ = Image.open('result_v7.png')
+                    os.system("python ./yolov7/detect.py --weights yolov7_best.pt --img 416 --conf 0.4 --source {}".format("upload.png"))
+                    #--Display predicton
+                    img_ = Image.open('./result_v7.png')
                     st.image(img_, caption='Plane Detection Yolov7')
                 
             col3, col4 = st.columns(2)
@@ -166,47 +119,37 @@ def imageInput(src):
                 if image_file is not None :
 
    
-                    model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=True) 
-                    model.cpu()
-                    pred = model("upload.png")
-                    pred.render()  # render bbox in image
-
-                    for im in pred.ims:
-                        im_base64 = Image.fromarray(im)
-                        im_base64.save("result.png")
-
-            #--Display predicton
-            
+                    os.system("python ./yolov5/detect.py --weights Best.pt --img 416 --conf 0.4 --source {}".format("upload.png"))
                     img_ = Image.open("result.png")
                     st.image(img_, caption='Plane Detection Yolov5')
                                         
-            col5, col6 = st.columns(2)
-            with col5:
+            # col5, col6 = st.columns(2)
+            # with col5:
+            #     img = Image.open(image_file)
+            #     st.image(img, caption='Selected Image', use_column_width='always')
+            # with col6:            
+            #     if image_file is not None and submit:
+            #         #--Display predicton
+            #         os.system("python ./yolor/detect.py --weights yolor_p6.pt --img 416 --conf 0.4 --device cpu --cfg ./yolor/cfg/yolor_p6.cfg --names ./yolor/data/coco.names --source {}".format(image_file))
+            #         img_ = Image.open("result_r.png")
+            #         st.image(img_, caption='Plane Detection YoloR')    
+
+            col7, col8 = st.columns(2)
+            with col7:
                 img = Image.open(image_file)
                 st.image(img, caption='Selected Image', use_column_width='always')
-            with col6:            
+            with col8:            
                 if image_file is not None :
-                    #--Display predicton
-                    os.system("python /app/planes_detection/yolor/detect.py --weights yolor_p6.pt --img 416 --conf 0.4 --device cpu --cfg ./yolor/cfg/yolor_p6.cfg --names ./yolor/data/coco.names --source {}".format(image_file))
-                    img_ = Image.open("result_r.png")
-                    st.image(img_, caption='Plane Detection YoloR')    
-
-#             col7, col8 = st.columns(2)
-#             with col7:
-#                 img = Image.open(image_file)
-#                 st.image(img, caption='Selected Image', use_column_width='always')
-#             with col8:            
-#                 if image_file is not None :
 
     
-#                     im= Image.open("upload.png")
-#                     outputs = predictor(im)
-#                     v = Visualizer(im,
-#                             metadata=my_dataset_train_metadata, 
-#                             scale=0.8
-#                              )
-#                     out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-#                     st.image(out.get_image(), caption='Plane Detection Faster rcnn')
+                    im=cv2.imread("upload.png")
+                    outputs = predictor(im)
+                    v = Visualizer(im[:, :, ::-1],
+                            metadata=my_dataset_train_metadata, 
+                            scale=0.8
+                             )
+                    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+                    st.image(out.get_image(), caption='Plane Detection Faster rcnn')
             
             
 
@@ -214,7 +157,7 @@ def imageInput(src):
     elif src == 'From test set.': 
         
         # Image selector slider
-        imgpath = glob.glob('./pic/*')
+        imgpath = glob.glob('.\pic\*')
         imgsel = st.slider('Select random images from test set.', min_value=1, max_value=len(imgpath), step=1) 
         image_file = imgpath[imgsel-1]
         submit = st.button("Detect")
@@ -251,24 +194,24 @@ def imageInput(src):
                
         
         
-#         col7, col8 = st.columns(2)
-#         with col7:
-#             img = Image.open(image_file)
-#             st.image(img, caption='Selected Image', use_column_width='always')
-#         with col8:            
-#             if image_file is not None and submit:
+        col7, col8 = st.columns(2)
+        with col7:
+            img = Image.open(image_file)
+            st.image(img, caption='Selected Image', use_column_width='always')
+        with col8:            
+            if image_file is not None and submit:
                 
 
-#                 #--Display predicton
-#                 im= Image.open(image_file)
-#                 # im = cv2.imread(imageName)
-#                 outputs = predictor(im)
-#                 v = Visualizer(im,
-#                         metadata=my_dataset_train_metadata, 
-#                         scale=0.8
-#                          )
-#                 out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-#                 st.image(out.get_image(), caption='Plane Detection Faster rcnn')
+                #--Display predicton
+                im=cv2.imread(image_file)
+                # im = cv2.imread(imageName)
+                outputs = predictor(im)
+                v = Visualizer(im[:, :, ::-1],
+                        metadata=my_dataset_train_metadata, 
+                        scale=0.8
+                         )
+                out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+                st.image(out.get_image(), caption='Plane Detection Faster rcnn')
 
         col5, col6 = st.columns(2)
         with col5:
