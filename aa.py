3cade590bdac4f1353ba9b2f4d5699a44b5b3373
nasser01001 <117510068+nasser01001@users.noreply.github.com>
2022-11-06 17:05:28 +0200

    sub

--- aa.py
+++ aa.py
@@ -18,6 +18,8 @@ import time
 
 
 from pathlib import Path
+import subprocess
+
 
 import sys
 import os
@@ -116,19 +118,19 @@ def imageInput(src):
             
                 if image_file is not None :
 
-
+                    subprocess.run(["python", "./yolov7/detect.py"])
 #                     os.system("python ./yolov7/detect.py --weights yolov7_best.pt --img 416 --conf 0.4 --source {}".format("upload.png"))
                     #--Display predicton
 #                     subprocess.run([f"{sys.executable}", "yolov7/detect.py --weights yolov7_best.pt --img 416 --conf 0.4 --source {}".format("upload.png")])
 #                     model = torch.hub.load('yolov7','custom',path="yolov7_best.pt",force_reload=True)
-                    model = attempt_load('yolov7_best.pt', map_location='cpu')
+                    #model = attempt_load('yolov7_best.pt', map_location='cpu')
 #                     model.cpu()
-                    pred = model("upload.png")
-                    pred.render()  # render bbox in image
+#                     pred = model("upload.png")
+#                     pred.render()  # render bbox in image
 
-                    for im in pred.imgs:
-                        im_base64 = Image.fromarray(im)
-                        im_base64.save("result_v7.png")
+#                     for im in pred.imgs:
+#                         im_base64 = Image.fromarray(im)
+#                         im_base64.save("result_v7.png")
                     img_ = Image.open('result_v7.png')
                     st.image(img_, caption='Plane Detection Yolov7')
                 
